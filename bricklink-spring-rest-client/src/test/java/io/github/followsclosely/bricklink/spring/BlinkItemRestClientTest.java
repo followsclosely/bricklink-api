04c583234e29a338c06495ce13a5c5521eaf94df
Matthew L. Wilson <mlavwilson@gmail.com>
2026-03-02 08:44:24 -0500

    Updated bricklink feeds

--- bricklink-spring-rest-client/src/test/java/io/github/followsclosely/bricklink/spring/BlinkItemRestClientTest.java
+++ bricklink-spring-rest-client/src/test/java/io/github/followsclosely/bricklink/spring/BlinkItemRestClientTest.java
@@ -25,7 +25,7 @@ class BlinkItemRestClientTest {
 
     @Test
     void testGetItem() throws Exception {
-        BlinkResponse<BlinkItem> response = client.getItem(BlinkItem.Type.SET, "col28-1");
+        BlinkResponse<BlinkItem> response = client.getItem(BlinkItem.Type.SET, "10468");
         assertNotNull(response);
         assertNotNull(response.getData());
         log.info("Retrieved item: {}", response.getData().getName());
